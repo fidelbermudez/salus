f22938e51d8778b8f2f8ca8f4a1f48d6c797ee61
Nicole Romero Ospina <nicole.romero.ospina@duke.edu>
2023-09-23 18:31:25 -0400

    created skeleton

---
+++ salus-app/src/pages/Bills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bills() {
+  return (
+    <div className="Bills">
+      <h1> Welcome to your Bills page </h1>
+    </div>
+  );
+}
+
+export default Bills;
---
+++ salus-app/src/pages/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Transactions() {
+  return (
+    <div className="transactions">
+      <h1> Welcome to your transaction history </h1>
+    </div>
+  );
+}
+
+export default Transactions;
