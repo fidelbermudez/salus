b86d4a59efd382b14d249029bda31f679fc4b89b
Kelvin Bueno <kelvinbueno@Kelvins-MacBook-Pro.local>
2023-09-25 15:57:58 -0400

    app skeleton minus backend/docker

---
+++ client/src/pages/budget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Budget() {
+  return (
+    <div className="Budget">
+      <h1> Welcome to your budget page </h1>
+    </div>
+  );
+}
+
+export default Budget;
