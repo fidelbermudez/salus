7315b5f981a65bff67237898f4a6b1d84f832e74
Nicole Romero Ospina <nicole.romero.ospina@duke.edu>
2023-09-27 01:27:09 -0400

    created modal for adding goals

--- client/src/pages/savings.jsx
+++ client/src/pages/savings.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import "../styles/savings.css"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+// import CloseButton from 'react-bootstrap/CloseButton';
+
+
+function NewGoalForm() {
+  return (
+   <Form>
+      <Form.Group className="mb-3" controlId="formGoalName">
+        <Form.Label>Goal Name</Form.Label>
+        <Form.Control type="goal-name" placeholder='e.g. Vacation' />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGoalAmount">
+        <Form.Label>Goal Amount</Form.Label>
+        <Form.Control type="goal-amount" placeholder=" e.g. 2000" />
+      </Form.Group>
+    </Form>
+  );
+}
+
+function NewGoalModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+    <Modal.Header>
+      <Modal.Title id="contained-modal-title-vcenter">
+          Add a New Savings Goal
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <NewGoalForm/>
+        <Button onClick={props.onHide}>Save Changes</Button>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
 
 function Savings() {
+  const [modalShow, setModalShow] = React.useState(false);
+  
   return (
     <div className="Saving">
       <h1> Savings </h1>
@@ -9,11 +55,20 @@ function Savings() {
       {/* box displaying total savings across all accounts */}
       <div className="total-savings"> 
         <h3>Total Savings </h3>
-        <p> test </p>
       </div>
 
+      <div className = "add">
       {/* button that allows user to add a new savings goal */}
-      <button className = "add"> Add new savings goal </button>
+      <Button variant="primary" className = "button" onClick={() => setModalShow(true)}>
+        Add new goal 
+      </Button>
+
+      <NewGoalModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      </div>
+
       
       {/* display of existing savings goals and goal progress */}
       <div className = "goals"> 
