bebac6ef453568760df061321472e135662c5fab
Johnny <jac188@duke.edu>
2023-10-04 15:30:58 -0400

    Revert "commented in budget page"
    This reverts commit c87c9186d32e23a645643775e2e1bd728343df59.

--- client/src/pages/budget.jsx
+++ client/src/pages/budget.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/Stack';
-import Container from 'react-bootstrap/Container';
+// import Button from 'react-bootstrap/Button';
+// import Stack from 'react-bootstrap/Stack';
+// import Container from 'react-bootstrap/Container';
 
 function Budget() {
   return (
-    <Container className="my-4">
-    <h1> Budgeting </h1>
-      <Stack direction="horizontal" gap="2" className="mb-4">
-      <h1 className="me-auto"> Current Budgets </h1>
-      <Button variant="primary">Create New Budget</Button>
-      </Stack>
-      <div style={{ 
-        display:"grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-        gap: "1rem",
-        alignItems: "flex-start",
-        }}
-      >
-      </div>
+    <></>
+    // <Container className="my-4">
+    // <h1> Budgeting </h1>
+    //   <Stack direction="horizontal" gap="2" className="mb-4">
+    //   <h1 className="me-auto"> Current Budgets </h1>
+    //   <Button variant="primary">Create New Budget</Button>
+    //   </Stack>
+    //   <div style={{ 
+    //     display:"grid",
+    //     gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+    //     gap: "1rem",
+    //     alignItems: "flex-start",
+    //     }}
+    //   >
+    //   </div>
     
-    </Container>
+    // </Container>
   );
 }
 
