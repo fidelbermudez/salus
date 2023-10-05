725693cf2ff70dbefc6e9bcc81e78dafa8240653
Johnny <jac188@duke.edu>
2023-10-05 05:42:39 -0400

    created budget and expense userid route

--- client/src/pages/summary.jsx
+++ client/src/pages/summary.jsx
@@ -1,62 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import BarGraph from '../components/barGraph.jsx'; // Import the 3D bar graph component
 
 function Summary() {
   const [userId, setUserId] = useState(4);
   const [expenseInfo, setExpenseInfo] = useState([]);
-  const [incomeInfo, setIncomeInfo] = useState([]);
+  const [budgetInfo, setBudgetInfo] = useState([]);
   const [error, setError] = useState(null);
-
 
   useEffect(() => {
     const fetchExpenseInfo = async () => {
       try {
-        const response = await axios.get('http://localhost:8081/api/expense/all');
+        const response = await axios.get(`http://localhost:8081/api/expense/user/${userId}`);
         console.log(response.data, "test1");
-
-        const filteredExpenses = response.data.filter((expense) => expense.user_id === userId);
-        console.log(filteredExpenses, "test3");
-
-        setExpenseInfo(filteredExpenses);
+        setExpenseInfo(response.data);
       } catch (e) {
         setError(e.message || 'Failed to fetch expense info');
         console.error(e);
       }
     };
 
-    const fetchIncomeInfo = async () => {
+    const fetchBudgetInfo = async () => {
       try {
-        const response = await axios.get('http://localhost:8081/api/income/all');
-        console.log(response.data, "test1");
-
-        const filteredIncomes = response.data.filter((income) => income.user_id === userId);
-        console.log(filteredIncomes, "test3");
-
-        setIncomeInfo(filteredIncomes);
+        const response = await axios.get(`http://localhost:8081/api/budgetSummary/user/${userId}`);
+        setBudgetInfo(response.data);
       } catch (e) {
-        setError(e.message || 'Failed to fetch income info');
+        setError(e.message || 'Failed to fetch budget info');
         console.error(e);
       }
-  };
+    };
 
-  fetchIncomeInfo();
-  fetchExpenseInfo();
-}, [userId]);
-
+    fetchBudgetInfo();
+    fetchExpenseInfo();
+  }, [userId]);
 
   return (
     <div>
       <h1> Welcome to your Summary!</h1>
       <ul>
         {expenseInfo.map((expense) => (
-          <li key={expense._id}>{expense.description}</li>
+          <li key={expense._id}>{expense.description}, {expense.category_id}, {expense.date} </li>
         ))}
       </ul>
       <ul>
-        {incomeInfo.map((income) => (
-          <li key={income._id}>{income.source}</li>
+        {budgetInfo.map((budget) => (
+          <li key={budget.budget_id}>{budget.limit}, {budget.category_id}</li>
         ))}
       </ul>
+      <p>test0</p>
+      <BarGraph/>
+      <p>test1</p>
     </div>
   );
 }
--- server/routes/budgetSummaryRoutes.js
+++ server/routes/budgetSummaryRoutes.js
@@ -4,6 +4,23 @@ const Budget = require('../models/budgetSummary.js');
 
 // Define bank account-related routes here
 
+router.get('/user/:userId', async (req, res) => {
+  try {
+    const userId = parseInt(req.params.userId);
+    
+    const userSummaries = await Budget.find({ user_id: userId });
+
+    if (!userSummaries || userSummaries.length === 0) {
+      return res.status(404).json({ message: 'User budget summaries not found' });
+    }
+
+    res.json(userSummaries);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 router.get('/show/:budgetId', async (req, res) => {
   try {
     const budgetId = parseInt(req.params.budgetId);
