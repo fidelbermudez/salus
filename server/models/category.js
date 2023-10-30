372d0491556a775df331272b4b37cb3caf70af70
fidel.bermudez <fidel.bermudez@duke.edu>
2023-10-29 23:20:40 -0400

    fixed category scheme

--- server/models/category.js
+++ server/models/category.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const cateogrySchema = new mongoose.Schema({
-    category_id: Number,
     month: Number,
     year: Number,
     category_name: String,
     user: Number,
-    user_defined: Boolean
+    amount_spent: Number,
+    limit: Number
 }, {collection: "category"});
 
 module.exports = mongoose.model('category', cateogrySchema);
